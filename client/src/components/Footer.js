e5fde772530135c4698ab28d352bad0f2121c449
Alexis De Leon <alexisdlr3009@hotmail.com>
2022-10-04 12:21:31 -0500

    Desing

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import logo from '../assets/logo2.png'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <img src={logo} alt='logo' />
+      <span>Made with ❤ by Alexis De Leon © 2022 </span>
+    </footer>
   )
 }
 
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../assets/logo.png'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='navbar'>
@@ -7,7 +8,33 @@ const Navbar = () => {
         <div className='logo'>
         <img src={logo} alt='logo'/>
         </div>
-        <div className='links'>links</div>
+        <div className='links'>
+          <Link className='link' to='/?cat=art'>
+            <h6>ART</h6>
+          </Link>
+          <Link className='link' to='/?cat=science'>
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className='link' to='/?cat=technology'>
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className='link' to='/?cat=cinema'>
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className='link' to='/?cat=design'>
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className='link' to='/?cat=food'>
+            <h6>FOOD</h6>
+          </Link>
+          <span>john</span>
+          <span>Logout</span>
+          <span className='write'>
+            <Link to='/write' className='link'>
+              Write
+            </Link>
+          </span>
+        </div>
       </div>
     </div>
   )
